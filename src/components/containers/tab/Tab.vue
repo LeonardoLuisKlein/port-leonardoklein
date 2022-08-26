3944ff80b78fcf155cf4f68dd0b6f8f452f24536
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2022-08-26 16:59:00 -0300

    Feat: title component without style

---
+++ src/components/containers/tab/Tab.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Titles type="h1" />
+  </div>
+</template>
+
+<script>
+import Titles from '@/components/micro/titles/Titles.vue';
+
+export default {
+    // eslint-disable-next-line
+    name: 'Tab',
+    components: {
+        Titles,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/micro/titles/Titles.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1 :class="nameClass" v-if="type === 'h1'">{{ title }}</h1>
+    <h2 :class="nameClass" v-else-if="type === 'h2'">{{ title }}</h2>
+    <h3 :class="nameClass" v-else-if="type === 'h3'">{{ title }}</h3>
+    <h4 :class="nameClass" v-else-if="type === 'h4'">{{ title }}</h4>
+    <h5 v-else-if="type === 'h5'">{{ title }}</h5>
+    <h6 v-else-if="type === 'h6'">{{ title }}</h6>
+  </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line
+    name: "Titles",
+    props:{
+      type: {
+            type: String,
+            require: true,
+        },
+        nameClass: {
+            type: String,
+        },
+        title:{
+            type: String,
+            default: 'null',
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./Titles.scss"
+</style>
