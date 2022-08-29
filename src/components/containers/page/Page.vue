92c88f45ce2fe21ce06eae50ccfb1d892e1586bb
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2022-08-29 14:25:30 -0300

    Feat: adding a principal Page component

---
+++ src/components/containers/page/Page.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="page">
+    <leftBar />
+    <Tab />
+  </div>
+</template>
+
+<script>
+import leftBar from '../leftBar/leftBar.vue'
+import Tab from '../tab/Tab.vue'
+export default {
+    // eslint-disable-next-line
+    name: 'Page',
+    components: {
+        leftBar, Tab
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import './Page.scss';
+</style>
