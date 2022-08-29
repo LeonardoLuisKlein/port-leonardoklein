4e65ce1a2cb73e53b3c99d931de5e4d4281f6cc6
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2022-08-29 11:00:37 -0300

    Feat: skiilCards component with basic style

--- src/components/containers/tab/Tab.vue
+++ src/components/containers/tab/Tab.vue
@@ -2,18 +2,20 @@
   <div>
     <Titles type="h1" />
     <Texts text="Teste" />
+    <skillCards />
   </div>
 </template>
 
 <script>
 import Titles from '@/components/micro/titles/Titles.vue';
 import Texts from '@/components/micro/texts/Texts.vue'
+import  skillCards from '@/components/micro/skillCards/skillCards.vue'
 
 export default {
     // eslint-disable-next-line
     name: 'Tab',
     components: {
-        Titles, Texts
+        Titles, Texts, skillCards
     }
 }
 </script>
---
+++ src/components/micro/skillCards/skillCards.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="@/assets/github.png"/>
+    <Texts text="Github"/>
+  </div>
+</template>
+
+<script>
+import Texts from '../texts/Texts.vue';
+export default {
+    name: "skillCards",
+    components: { Texts }
+}
+</script>
+
+<style scoped lang="scss">
+@import './skillCards.scss';
+</style>
