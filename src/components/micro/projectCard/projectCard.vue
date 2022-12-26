13c2ef8c8dbef2f1bed13cf1b30350d109e4a3e1
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2022-12-26 12:55:24 -0300

    Feat: adding projects image and creating the project card component

---
+++ src/components/micro/projectCard/projectCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="cardContainer">
+      <h3 id="titleCard">{{ cardText }}</h3>
+      <p id="desciptionCard">{{ description }}</p>
+    <img :src="resolve_img_url(picture_src)" id="imageCard" />
+    <div id="buttonsContainer">
+    <a :href="link"><button id="buttonCard">Visitar</button></a>
+    <a :href="linkRep"><button id="buttonCard">Repositório</button></a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: "projectCard",
+  props: {
+    cardText: String,
+    link: String,
+    description: String,
+    picture_src: String,
+    linkRep: String,
+  },
+  methods: {
+    resolve_img_url: function (path) {
+      let images = require.context('../../../assets/', false, /\.png$|\.jpg$/)
+      return images("./"+path)
+    },
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import './projectCard.scss';
+</style>
