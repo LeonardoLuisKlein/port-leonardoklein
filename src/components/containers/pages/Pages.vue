e8606a7ca9bad7d3872f80e1ab359e5363ed61a0
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2023-08-07 16:45:08 -0300

    Fix: bug fix

--- src/components/containers/pages/Pages.vue
+++ src/components/containers/pages/Pages.vue
@@ -2,7 +2,7 @@
     <div id="pages">
         <leftBar />
     <div id="tab">
-        <Titles type="h1" title="SALMOS 37" nameClass="titleCenter"/>
+    <Titles type="h1" title="SALMOS 37" nameClass="titleCenter"/>
     <br />
     <Texts nameClass="centerText" text="5: Entrega o teu caminho ao Senhor; confia nele, e ele o fará."/>
     <br/>
@@ -11,6 +11,17 @@
     <Texts nameClass="centerText" text="7: Descansa no Senhor, e espera nele; não te indignes por causa daquele que prospera em seu caminho, por causa do homem que executa astutos intentos."/>
     <br/>
     <Texts nameClass="centerText" text="8: Deixa a ira, e abandona o furor; não te indignes, porque isso só leva ao mal."/>
+    <br/>
+    <Titles :type="h2" title="Oração para São Cipriano" nameClass="titleCenter"/>
+    <br/>
+    <Texts nameClass="centerText" text="Pelos poderes de São Cipriano, eu (diga seu nome) vou conseguir de imediato um emprego bom durante o dia e com um ótimo ordenado.
+
+São Cipriano fazei pois que este pedido se realize de imediato e o emprego bom e com um bom salário surja agora sem demora pelos poderes de São Cipriano.
+
+
+Eu (diga seu nome) vou agora ter um emprego fixo em uma boa empresa ou outra oportunidade e agora em minhas mãos amarrado para nunca mais fugir de mim.
+
+Irei publicar de imediato essa oração para seu nome ser lembrado, reconhecido e respeitado, em troca de conseguir um bom emprego e um bom ordenado, que me dê o meu bom salário para eu me sustentar, para pagar todas as despesas fixas e dividas e eventuais, que ainda sobre para o meu lazer e também para poupar e manter uma reserva segura no banco, para nunca mais passar nenhuma dificuldade. De imediato durante esse momento, o emprego urgente peço, suplico urgente as treze entidades e as três almas que acompanham São Cipriano, que me atendam o mais rápido e urgente, que eu tenha esse emprego ainda essa semana e nunca mais fique desempregado, e a partir de agora em nome de São Cipriano, das treze entidades e das três almas que vigiam São Cipriano, que a partir da hora que eu fizer essa oração e que eu publicar, aconteça e assim seja! E assim será! E assim está feito! Amém."/>
 </div>
 </div>
   </template>
